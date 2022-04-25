c527bb5f3ed8567085caa8b3f86d32792db0dc7d
Matthias Fulz <mfulz@olznet.de>
2022-04-25 02:27:35 +0200

    Mitm basic implementation

--- kpxc-serveri-mitm.go
+++ kpxc-serveri-mitm.go
@@ -11,168 +11,158 @@ import (
 	"github.com/jamesruan/sodium"
 )
 
-type MitmReq struct {
-	ActionName string `json:"action"`
-	Nonce      string `json:"nonce"`
-	ClientId   string `json:"clientID"`
-	RequestId  string `json:"requestID"`
-	Message    string `json:"message"`
-	PublicKey  string `json:"publicKey"`
-	Success    string `json:"success"`
-	Error      string `json:"error"`
-	ErrorCode  string `json:"errorCode"`
-	Version    string `json:"version"`
-	data       interface{}
-	nonce      sodium.BoxNonce
+type KpXcMitmI interface {
+	ModifyReq(*ConnMsg) error
+	ModifyRes(*ConnMsg) error
 }
 
-func (m *MitmReq) Parse(breq []byte) (err error) {
-	if err = json.Unmarshal(breq, m); err != nil {
+type kpXcModifier struct {
+	keyPair      sodium.BoxKP
+	clientPubKey sodium.BoxPublicKey
+	serverPubKey sodium.BoxPublicKey
+	modifier     KpXcMitmI
+}
+
+type KpXcMitm struct {
+	conn     ConnectionI
+	modifier *kpXcModifier
+}
+
+func (m *kpXcModifier) ModifyReq(req *ConnMsg) (err error) {
+	encrypt := false
+
+	switch req.ActionName {
+	case "change-public-keys":
+		if m.clientPubKey.Bytes, err = base64.StdEncoding.DecodeString(req.PublicKey); err != nil {
+			return err
+		}
+		req.PublicKey = base64.StdEncoding.EncodeToString(m.keyPair.PublicKey.Bytes)
+		break
+	default:
+		if req.Message != "" && req.Nonce != "" && req.data != nil {
+			jedata, err := base64.StdEncoding.DecodeString(req.Message)
+			if err != nil {
+				return err
+			}
+			jdata, err := DecryptBytes(req.nonce, m.clientPubKey, m.keyPair.SecretKey, jedata)
+			if err != nil {
+				return err
+			}
+			if err = json.Unmarshal(jdata, req.data); err != nil {
+				return err
+			}
+
+			if req.ActionName == "associate" {
+				req.data.(*MsgAssociate).Key = base64.StdEncoding.EncodeToString(m.keyPair.PublicKey.Bytes)
+			}
+			encrypt = true
+		}
+		break
+	}
+
+	if err = m.modifier.ModifyReq(req); err != nil {
 		return err
 	}
 
-	if m.Nonce != "" {
-		m.nonce.Bytes, err = base64.StdEncoding.DecodeString(m.Nonce)
+	if encrypt {
+		jdata, err := json.Marshal(req.data)
 		if err != nil {
 			return err
 		}
+		jedata := EncryptBytes(req.nonce, m.serverPubKey, m.keyPair.SecretKey, jdata)
+		req.Message = base64.StdEncoding.EncodeToString(jedata)
 	}
 
 	return nil
 }
 
-type KpXcMitm struct {
-	conn         ConnectionI
-	keyPair      sodium.BoxKP
-	clientPubKey sodium.BoxPublicKey
-	serverPubKey sodium.BoxPublicKey
-}
+func (m *kpXcModifier) ModifyRes(res *ConnMsg) (err error) {
+	encrypt := false
 
-func (k *KpXcMitm) pocHandleEncReq(req *MitmReq) (hres []byte, err error) {
-	//res := &MitmReq{}
-
-	//switch req.ActionName {
-	//case "get-databasehash":
-	//req.data = &MsgGetDatabasehash{}
-	//res.data = &MsgGetDatabasehash{}
-	//break
-	//case "associate", "test-associate":
-	//req.data = &MsgAssociate{}
-	//res.data = &MsgAssociate{}
-	//break
-	//}
-
-	//if req.Message != "" && req.Nonce != "" {
-	//slog.LOG_DEBUGF("req: ")
-	//if err = Decrypt(req.nonce, k.clientPubKey, k.keyPair.SecretKey, req.Message, req.data); err != nil {
-	//return hres, err
-	//}
-	////slog.LOG_DEBUGF("decrypted req: %v\n", req.data)
-	//if req.ActionName == "associate" {
-	//req.data.(*MsgAssociate).Key = base64.StdEncoding.EncodeToString(k.keyPair.PublicKey.Bytes)
-	//}
-	//if req.Message, err = Encrypt(req.nonce, k.serverPubKey, k.keyPair.SecretKey, req.data); err != nil {
-	//return hres, err
-	//}
-	//}
-
-	//jreq, err := json.Marshal(req)
-	//if err != nil {
-	//return hres, err
-	//}
-	//if err = k.conn.Send(jreq); err != nil {
-	//return hres, err
-	//}
-
-	//jres, err := k.conn.Recv(BufSize, 0)
-	//if err != nil {
-	//return hres, err
-	//}
-	////slog.LOG_DEBUGF("jres: %v\n", jres)
-	////slog.LOG_DEBUGF("jres: %s\n", jres)
-	//if err = res.Parse(jres); err != nil {
-	//return hres, err
-	//}
-
-	//if res.Message != "" && res.Nonce != "" {
-	//slog.LOG_DEBUGF("res: ")
-	//if err = Decrypt(res.nonce, k.serverPubKey, k.keyPair.SecretKey, res.Message, res.data); err != nil {
-	//return hres, err
-	//}
-	////slog.LOG_DEBUGF("decrypted res: %v\n", res.data)
-	//if res.Message, err = Encrypt(res.nonce, k.clientPubKey, k.keyPair.SecretKey, res.data); err != nil {
-	//return hres, err
-	//}
-	//}
-
-	//return json.Marshal(res)
-	return nil, fmt.Errorf("not implemented")
-}
-
-func (k *KpXcMitm) handleChangePubKeys(req *MitmReq) (hreq []byte, err error) {
-	if k.clientPubKey.Bytes, err = base64.StdEncoding.DecodeString(req.PublicKey); err != nil {
-		return hreq, err
+	switch res.ActionName {
+	case "change-public-keys":
+		if m.serverPubKey.Bytes, err = base64.StdEncoding.DecodeString(res.PublicKey); err != nil {
+			return err
+		}
+		res.PublicKey = base64.StdEncoding.EncodeToString(m.keyPair.PublicKey.Bytes)
+		break
+	default:
+		if res.Message != "" && res.Nonce != "" && res.data != nil {
+			jedata, err := base64.StdEncoding.DecodeString(res.Message)
+			if err != nil {
+				return err
+			}
+			jdata, err := DecryptBytes(res.nonce, m.serverPubKey, m.keyPair.SecretKey, jedata)
+			if err != nil {
+				return err
+			}
+			if err = json.Unmarshal(jdata, res.data); err != nil {
+				return err
+			}
+			encrypt = true
+		}
+		break
 	}
 
-	req.PublicKey = base64.StdEncoding.EncodeToString(k.keyPair.PublicKey.Bytes)
+	if err = m.modifier.ModifyRes(res); err != nil {
+		return err
+	}
+
+	if encrypt {
+		jdata, err := json.Marshal(res.data)
+		if err != nil {
+			return err
+		}
+		jedata := EncryptBytes(res.nonce, m.clientPubKey, m.keyPair.SecretKey, jdata)
+		res.Message = base64.StdEncoding.EncodeToString(jedata)
+	}
+
+	return nil
+}
+
+func (k *KpXcMitm) HandleReq(breq []byte) (bres []byte, err error) {
+	req, err := ParseConnMsg(breq)
+	if err != nil {
+		return bres, err
+	}
+
+	if err = k.modifier.ModifyReq(req); err != nil {
+		return bres, err
+	}
 
 	jreq, err := json.Marshal(req)
 	if err != nil {
-		return hreq, err
+		return bres, err
 	}
+
 	if err = k.conn.Send(jreq); err != nil {
-		return hreq, err
+		return bres, err
 	}
 
 	jres, err := k.conn.Recv(BufSize, 0)
 	if err != nil {
-		return hreq, err
-	}
-	res := &MitmReq{}
-	if err = res.Parse(jres); err != nil {
-		return hreq, err
+		return bres, err
 	}
 
-	if k.serverPubKey.Bytes, err = base64.StdEncoding.DecodeString(res.PublicKey); err != nil {
-		return hreq, err
+	res, err := ParseConnMsg(jres)
+	if err != nil {
+		return bres, err
 	}
 
-	res.PublicKey = base64.StdEncoding.EncodeToString(k.keyPair.PublicKey.Bytes)
+	if err = k.modifier.ModifyRes(res); err != nil {
+		return bres, err
+	}
 
 	return json.Marshal(res)
 }
 
-func (k *KpXcMitm) HandleReq(breq []byte) (res []byte, err error) {
-	//slog.LOG_DEBUGF("breq: %v\n", breq)
-	//slog.LOG_DEBUGF("breq: %s\n", breq)
-
-	req := &MitmReq{}
-	if err = req.Parse(breq); err != nil {
-		return res, err
-	}
-
-	if req.ActionName == "change-public-keys" {
-		return k.handleChangePubKeys(req)
-	}
-
-	return k.pocHandleEncReq(req)
-
-	if err = k.conn.Send(breq); err != nil {
-		//slog.LOG_DEBUGF("err: %v\n", err)
-		return res, err
-	}
-
-	if res, err = k.conn.Recv(BufSize, 0); err != nil {
-		return res, err
-	}
-	//slog.LOG_DEBUGF("res: %v\n", res)
-	//slog.LOG_DEBUGF("res: %s\n", res)
-
-	return res, err
-}
-
-func NewKpXcMitm() (ret *KpXcMitm, err error) {
+func NewKpXcMitm(modifier KpXcMitmI) (ret *KpXcMitm, err error) {
+	mod := new(kpXcModifier)
+	mod.modifier = modifier
+	mod.keyPair = sodium.MakeBoxKP()
 	serv := new(KpXcMitm)
+	serv.modifier = mod
+
 	var serverAddress string
 
 	tmpDir := os.Getenv("TMPDIR")
@@ -202,11 +192,9 @@ func NewKpXcMitm() (ret *KpXcMitm, err error) {
 		return serv, fmt.Errorf("Operating System: '%s' not supported", oss)
 	}
 
-	//slog.LOG_DEBUGF("serverAddress: %s\n", serverAddress)
 	if err = serv.conn.Connect(serverAddress); err != nil {
 		return serv, err
 	}
-	serv.keyPair = sodium.MakeBoxKP()
 	ret = serv
 
 	return ret, err
